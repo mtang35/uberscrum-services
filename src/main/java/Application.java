b5539085d62ad682df1fbc6cbb5f006dba694004
Tang, Michael (Bothell) <Michael.Tang2@T-Mobile.com>
2017-07-02 16:51:00 -0700

    Added DailyScrum classes

---
+++ src/main/java/Application.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package main.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mtang1
+ *
+ */
+
+@SpringBootApplication
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+
+}
